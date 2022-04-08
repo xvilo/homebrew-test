ddbc730e8598f585548050f7f4e05846b30d52ad
Sem Schilder <sem@tropical.email>
2022-04-08 15:54:37 +0200

    Add WF

---
+++ .github/scripts/edit.sh
@@ -0,0 +1,16 @@
+get_release() {
+  curl -sL "https://github.com/transip/tipctl/tags" | grep -Po -m 1 "v[0-9]+\.[0-9]+\.[0-9]+$" | head -n 1
+}
+
+fetch() {
+  REMOTE_TIPCTL_SEMVER=$(get_release)
+  OLD_TIPCTL_SEMVER=$(grep -Po -m 1 "v[0-9]+\.[0-9]+\.[0-9]+" ./Formula/tipctl.rb)
+  if [ "$REMOTE_TIPCTL_SEMVER" != "$OLD_TIPCTL_SEMVER" ]; then
+    sed -i -e "s|$OLD_TIPCTL_SEMVER|$REMOTE_TIPCTL_SEMVER|g" ./Formula/tipctl.rb
+    url="$(grep -e "^  url.*" ./Formula/tipctl.rb | cut -d\" -f 2)"
+    checksum=$(curl -sSL "$url" | shasum -a 256 | cut -d' ' -f 1)
+    sed -i -e "s|^  sha256.*|  sha256 \"$checksum\"|g" ./Formula/tipctl.rb
+  fi
+}
+
+fetch
